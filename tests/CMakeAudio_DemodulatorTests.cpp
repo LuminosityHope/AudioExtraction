90dbe52b8a35a885b8921592a8eac4fae14142ce
LuminosityHope <dedhek@mail.ru>
2025-07-01 20:02:34 +0300

    Add files via upload

---
+++ tests/CMakeAudio_DemodulatorTests.cpp
@@ -0,0 +1,62 @@
+﻿#include "gtest/gtest.h"
+#include "Demodulator.h" 
+#include <ccomplex>
+#include <math.h>
+#include <vector>
+float const PI = 3.1415927f;
+class DemodulatorTest : public::testing::Test
+{
+protected:
+	virtual void SetUp(void)
+	{
+		dataFloat = { {1.1f,0.2f}, {2.3f, 4.1f}, {6.9f,4.5f} }; 
+		dataInt = { {1,2 }, {5, 4}, {7,9}, {6,0} }; 
+	}
+	std::vector<std::complex<float>> dataFloat;
+	std::vector<std::complex<int>>dataInt;
+};
+TEST_F(DemodulatorTest, DataCorrectlyAM)
+{
+	DemodulatorAM<int> demodInt;
+	auto demod = demodInt.demodulate(dataInt);
+	ASSERT_EQ(demod.size(), dataInt.size());
+	EXPECT_EQ(demod[0], round(sqrt(1 * 1 + 2 * 2)));
+	EXPECT_EQ(demod[1], round(sqrt(5 * 5 + 4 * 4)));
+	EXPECT_EQ(demod[2], round(sqrt(7 * 7 + 9 * 9)));
+	EXPECT_EQ(demod[3], round(sqrt(6 * 6 + 0 * 0)));
+
+	DemodulatorAM<float> demodFloat;
+	auto demodFl = demodFloat.demodulate(dataFloat);
+	ASSERT_EQ(demodFl.size(), dataFloat.size());
+	EXPECT_FLOAT_EQ(demodFl[0], (sqrtf(1.1f * 1.1f + 0.2f * 0.2f)));
+	EXPECT_FLOAT_EQ(demodFl[1], (sqrtf(2.3f * 2.3f + 4.1f * 4.1f)));
+	EXPECT_FLOAT_EQ(demodFl[2], (sqrtf(6.9f * 6.9f + 4.5f * 4.5f)));
+}
+TEST_F(DemodulatorTest, DataCorrectlyFM)
+{
+	DemodulatorFM<int> demodInt;
+	auto demod = demodInt.demodulate(dataInt);
+	ASSERT_EQ(demod.size(), dataInt.size() - 1);
+
+	EXPECT_EQ(demod[0], (std::arg(std::complex<int>(5, 4) * std::conj(std::complex<int>(1, 2)))));
+	EXPECT_EQ(demod[1], (std::arg(std::complex<int>(7, 9) * std::conj(std::complex<int>(5, 4)))));
+	EXPECT_EQ(demod[2], (std::arg(std::complex<int>(6, 0) * std::conj(std::complex<int>(7, 9)))));
+
+	DemodulatorFM<float> demodF;
+	auto demodFl = demodF.demodulate(dataFloat);
+	ASSERT_EQ(demod.size(), dataInt.size() - 1);
+
+	EXPECT_FLOAT_EQ(demodFl[0], (std::arg(std::complex<float>(2.3f, 4.1f) * std::conj(std::complex<float>(1.1f, 0.2f)))));
+	EXPECT_FLOAT_EQ(demodFl[1], (std::arg(std::complex<float>(6.9f, 4.5f) * std::conj(std::complex<float>(2.3f, 4.1f)))));
+}
+TEST_F(DemodulatorTest, DiffPhase) {
+	std::vector<std::complex<float>> signal = {
+		{1.0f, 0.0f}, 
+		{0.0f, 1.0f}   
+	};
+
+	DemodulatorFM<float> demod;
+	auto result = demod.demodulate(signal);
+
+	EXPECT_FLOAT_EQ(result[0],PI/2);
+}
---
+++ tests/CMakeAudio_ReadTests.cpp
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+#include "FileManager.h" 
+#include <fstream>
+#include <filesystem>
+
+template<typename T>
+void CreateTestFile(const std::string& fileName, const std::vector<T>& data) {
+    std::ofstream file(fileName, std::ios::binary);
+    file.write(reinterpret_cast<const char*>(data.data()), data.size() * sizeof(T));
+}
+
+class ReadSignalTest : public ::testing::Test {
+protected:
+    virtual void SetUp(void)  {
+    
+        std::vector<float> goodData = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f };
+        CreateTestFile("good_file.bin", goodData);
+
+        std::vector<float> badData = { 1.0f, 2.0f, 3.0f }; 
+        CreateTestFile("bad_file.bin", badData);
+    }
+
+   virtual void TearDown(void)  {
+        
+        std::filesystem::remove("good_file.bin");
+        std::filesystem::remove("bad_file.bin");
+    }
+};
+
+ 
+TEST_F(ReadSignalTest, ReadsFileCorrectly) {
+    auto result = FileManager::ReadSignal<float>("good_file.bin");
+
+    ASSERT_EQ(result.size(), 3);
+    EXPECT_FLOAT_EQ(result[0].real(), 1.0f);
+    EXPECT_FLOAT_EQ(result[0].imag(), 2.0f);
+    EXPECT_FLOAT_EQ(result[1].real(), 3.0f);
+    EXPECT_FLOAT_EQ(result[1].imag(), 4.0f);
+    EXPECT_FLOAT_EQ(result[2].real(), 5.0f);
+    EXPECT_FLOAT_EQ(result[2].imag(), 6.0f);
+}
+
+
+TEST_F(ReadSignalTest, ThrowsOnNonexistentFile) {
+    EXPECT_THROW({
+         FileManager::ReadSignal<float>("nonexistent_file.bin");
+        }, std::runtime_error);
+}
+
+TEST_F(ReadSignalTest, ThrowsOnOddSize) {
+    EXPECT_THROW({
+         FileManager::ReadSignal<float>("bad_file.bin");
+        }, std::runtime_error);
+}
