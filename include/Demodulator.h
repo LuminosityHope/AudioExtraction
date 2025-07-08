14a8094548258a1b7337dc302336d153548a130b
LuminosityHope <dedhek@mail.ru>
2025-07-08 12:27:44 +0300

    Update Demodulator.h

--- include/Demodulator.h
+++ include/Demodulator.h
@@ -7,25 +7,26 @@ template <typename T>
 class Demodulator
 {
 public:
-	virtual ~Demodulator() = default;
-	virtual std::vector<T> demodulate(const std::vector<std::complex<T>>& signal) = 0;
+    virtual ~Demodulator() = default;
+    virtual std::vector<float> demodulate(const std::vector<std::complex<T>>& signal) = 0;
 };
+
 
 template <typename T>
 class DemodulatorAM :public Demodulator<T>
 {
 public:
-   ~DemodulatorAM() override = default;
-    std::vector<T> demodulate(const std::vector<std::complex<T>>& signal)
+    ~DemodulatorAM() override = default;
+    std::vector<float> demodulate(const std::vector<std::complex<T>>& signal)
     {
-        std::vector<T> demodulatedSignal;
+        std::vector<float> demodulatedSignal;
         demodulatedSignal.reserve(signal.size());
 
         for (size_t i = 0; i < signal.size(); ++i)
         {
             float real = static_cast<float>(signal[i].real());
             float imag = static_cast<float>(signal[i].imag());
-            float module = std::sqrtf(real * real + imag * imag);
+            double module = std::sqrtf(real * real + imag * imag);
 
             if (std::is_integral<T>::value)
             {
@@ -44,9 +45,9 @@ class DemodulatorFM : public Demodulator<T>
 {
 public:
 	~DemodulatorFM() override = default;
-	std::vector<T> demodulate(const std::vector<std::complex<T>>& signal)
+	std::vector<float> demodulate(const std::vector<std::complex<T>>& signal)
 	{
-		std::vector<T> demodulatedSignal;
+		std::vector<float> demodulatedSignal;
 		demodulatedSignal.reserve(signal.size() - 1);
 
 		for (size_t i = 1; i <= signal.size() - 1; ++i)
