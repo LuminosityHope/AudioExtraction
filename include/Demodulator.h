d2061ae9f4fe7bc7cdbfe18a952e395316efaf48
LuminosityHope <dedhek@mail.ru>
2025-07-08 12:34:45 +0300

    Update Demodulator.h

--- include/Demodulator.h
+++ include/Demodulator.h
@@ -26,7 +26,7 @@ public:
         {
             float real = static_cast<float>(signal[i].real());
             float imag = static_cast<float>(signal[i].imag());
-            double module = std::sqrtf(real * real + imag * imag);
+            float module = std::sqrtf(real * real + imag * imag);
 
             if (std::is_integral<T>::value)
             {
