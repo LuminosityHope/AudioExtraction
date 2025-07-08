b0eed533d56bc1e3f8e5e2f4d8a204d87643aad5
LuminosityHope <dedhek@mail.ru>
2025-07-08 12:52:53 +0300

    Update FromInt32ToFloat.cpp

--- src/FromInt32ToFloat.cpp
+++ src/FromInt32ToFloat.cpp
@@ -7,12 +7,11 @@ std::vector<float> convertToFloat(const std::vector<float>& signal) {
         return {};
     }
 
-    int32_t intmaxVal = *std::max_element(signal.begin(), signal.end(),
-        [](int32_t a, int32_t b) { return std::abs(a) < std::abs(b); });
-    float maxVal = static_cast<float>(intmaxVal);
+    float maxVal = *std::max_element(signal.begin(), signal.end(),
+        [](float a, float b) { return std::abs(a) < std::abs(b); });
     std::vector<float> result(signal.size());
     for (size_t i = 0; i < signal.size(); ++i) {
-        result[i] = static_cast<float>(signal[i]) / maxVal;
+        result[i] = (signal[i]) / maxVal;
     }
     return result;
 }
